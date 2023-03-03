4740724878892a670416358e55368c22dc160383
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2023-03-03 18:50:49 +0500

    Created: home_page.dart home_page_posts.dart home_bottom_bar.dart
    Added .jpg and .png files

---
+++ lib/widgets/home_page_posts.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePagePosts extends StatelessWidget {
+  const HomePagePosts({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Flash Sale",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          GridView.count(
+            crossAxisCount: 2,
+            physics: NeverScrollableScrollPhysics(),
+            childAspectRatio: 0.7,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            shrinkWrap: true,
+            children: [
+              for (int i = 1; i < 9; i++)
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          // Navigator.pushNamed(context, ItemPage());
+                        },
+                        child: Image.asset(
+                          "assets/images/$i.png",
+                          fit: BoxFit.contain,
+                          height: 130,
+                          width: 120,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Item Name",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: const [
+                                Text(
+                                  "\$50",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFFFF8608),
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  "/ 1KG",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Spacer(),
+                                Icon(
+                                  Icons.favorite_border,
+                                  color: Color(0xFFFF8608),
+                                  size: 28,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
